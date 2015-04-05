a4dfa24cda06bad108a78e7f1f53e5c3295d122a
OpenFibers <openfibers@gmail.com>
2015-04-05 20:25:16 +0800

    added server list view controller

---
+++ SSHMole/SMServerListViewController/SMServerListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMServerListViewController.h
+//  SSHMole
+//
+//  Created by openthread on 4/5/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMServerListViewController : NSViewController
+
+@end
---
+++ SSHMole/SMServerListViewController/SMServerListViewController.m
@@ -0,0 +1,22 @@
+//
+//  SMServerListViewController.m
+//  SSHMole
+//
+//  Created by openthread on 4/5/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import "SMServerListViewController.h"
+
+@interface SMServerListViewController ()
+
+@end
+
+@implementation SMServerListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
