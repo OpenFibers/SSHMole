672ce074d71ec07cdad4503da1e253ad553ba775
OpenFibers <openfibers@gmail.com>
2015-04-05 20:04:50 +0800

    init arch modified

---
+++ SSHMole/SMServerConfigSplitController/SMServerConfigSplitController.h
@@ -0,0 +1,13 @@
+//
+//  SMServerConfigSplitController.h
+//  SSHMole
+//
+//  Created by openthread on 4/5/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMServerConfigSplitController : NSSplitViewController
+
+@end
---
+++ SSHMole/SMServerConfigSplitController/SMServerConfigSplitController.m
@@ -0,0 +1,22 @@
+//
+//  SMServerConfigSplitController.m
+//  SSHMole
+//
+//  Created by openthread on 4/5/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import "SMServerConfigSplitController.h"
+
+@interface SMServerConfigSplitController ()
+
+@end
+
+@implementation SMServerConfigSplitController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
