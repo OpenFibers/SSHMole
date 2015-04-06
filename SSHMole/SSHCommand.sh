c61381dbaca29582da57bd17d1f600ab89629059
OpenFibers <openfibers@gmail.com>
2015-04-06 16:16:22 +0800

    handle no route situation in shell script

--- SSHMole/SSHCommand.sh
+++ SSHMole/SSHCommand.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/expect -f
 #!/bin/sh
 
-# Copyright (C) 2008  Antoine Mercadal
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 set arguments [lindex $argv 0]
 set password [lindex $argv 1]
 
@@ -30,6 +14,7 @@ set timeout 1
 set timeout -1
 expect {
 		"?sh: Error*" {puts "CONNECTION_ERROR"; exit};
+		"*o route to host*" {puts "NO_ROUTE_TO_HOST"; exit};
 		"*yes/no*" {send "yes\r"; exp_continue};
 		"*Connection refused*" {puts "CONNECTION_REFUSED"; exit};
 		"*?assword:*" {	send "$password\r"; set timeout 4;
