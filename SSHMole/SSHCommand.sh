114b710fb7ec41d91dfee501d696f961b17c4fd0
OpenFibers <openfibers@gmail.com>
2015-04-06 16:46:48 +0800

    SSHCommand updated

--- SSHMole/SSHCommand.sh
+++ SSHMole/SSHCommand.sh
@@ -15,8 +15,11 @@ set timeout -1
 expect {
 		"?sh: Error*" {puts "CONNECTION_ERROR"; exit};
 		"*o route to host*" {puts "NO_ROUTE_TO_HOST"; exit};
-		"*yes/no*" {send "yes\r"; exp_continue};
+		"*ad dynamic forwarding specification*" {puts "BAD_DYNAMIC_FORWARDING_SPECIFICATION"; exit};
+		"*sage*" {puts "SSH_SYNTAX_ERROR"; exit};
+		"*ad port*" {puts "BAD_LOCAL_PORT"; exit};
 		"*Connection refused*" {puts "CONNECTION_REFUSED"; exit};
+		"*yes/no*" {send "yes\r"; exp_continue};
 		"*?assword:*" {	send "$password\r"; set timeout 4;
 						expect "*?assword:*" {puts "WRONG_PASSWORD"; exit;}
 					  };
