a8605a9a7a29e41d2d3748a5a7b52da1463552ec
openthread-mini <openfibers@gmail.com>
2015-04-10 11:44:16 +0800

    added server list logic

--- SSHMole/SMServerListView/SMServerListView.m
+++ SSHMole/SMServerListView/SMServerListView.m
@@ -8,6 +8,7 @@
 
 #import "SMServerListView.h"
 #import "NSView+Vibrancy.h"
+#import "SMServerConfigStorage.h"
 
 @interface SMServerListView () <NSTableViewDataSource, NSTableViewDelegate>
 
@@ -16,31 +17,63 @@
 @implementation SMServerListView
 {
     NSView *_innerXibTableView;
+    NSArray *_serverConfigs;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     if ((self = [super initWithCoder:aDecoder]))
     {
-        NSArray *array = nil;
-        [[NSBundle mainBundle] loadNibNamed:@"ServerListTable" owner:self topLevelObjects:&array];
-        for (NSView *subObject in array)
-        {
-            if ([subObject isKindOfClass:[NSView class]])
-            {
-                _innerXibTableView = subObject;
-                _innerXibTableView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-                [self addSubview:subObject];
-                break;
-            }
-        }
+        [self loadView];
+        [self reloadData];
     }
     return self;
 }
 
+- (void)loadView
+{
+    NSArray *array = nil;
+    [[NSBundle mainBundle] loadNibNamed:@"ServerListTable" owner:self topLevelObjects:&array];
+    for (NSView *subObject in array)
+    {
+        if ([subObject isKindOfClass:[NSView class]])
+        {
+            _innerXibTableView = subObject;
+            _innerXibTableView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+            [self addSubview:subObject];
+            break;
+        }
+    }
+}
+
+- (void)reloadData
+{
+    _serverConfigs = [[SMServerConfigStorage defaultStorage] configs];
+}
+
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
 {
-    return 10;
+    return _serverConfigs.count + 1;
+}
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSString *columnIdentifier = [tableColumn identifier];
+    if ([columnIdentifier isEqualToString:@"ServerListColumn"])
+    {
+        NSTableCellView *cellView = [tableView makeViewWithIdentifier:@"ServerListCellView" owner:self];
+        if (row >= _serverConfigs.count)//View for "Add new config"
+        {
+            [cellView.textField setStringValue:@"Add new config"];
+        }
+        else//View for added configs
+        {
+            SMServerConfig *config = _serverConfigs[row];
+            [cellView.textField setStringValue:config.serverName];
+        }
+        return cellView;
+    }
+    return nil;
 }
 
 @end
