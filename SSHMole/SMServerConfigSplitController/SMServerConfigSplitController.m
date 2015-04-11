3f23eb9e78f7183c819ea12c2ea0770e553ca900
OpenFibers <openfibers@gmail.com>
2015-04-12 01:58:08 +0800

    set values for config view

--- SSHMole/SMServerConfigSplitController/SMServerConfigSplitController.m
+++ SSHMole/SMServerConfigSplitController/SMServerConfigSplitController.m
@@ -48,6 +48,13 @@
 {
     [self.serverConfigView setServerConfig:nil];
     self.currentConfig = nil;
+    
+    self.serverConfigView.serverAddressString = @"";
+    self.serverConfigView.serverPort = 0;//server config view will use default 22 port
+    self.serverConfigView.accountString = @"";
+    self.serverConfigView.passwordString = @"";
+    self.serverConfigView.localPort = 0;//server config view will use default 7070 port
+    self.serverConfigView.remarkString = @"";
 }
 
 //User did select existing server config.
@@ -55,6 +62,13 @@
 {
     [self.serverConfigView setServerConfig:config];
     self.currentConfig = config;
+    
+    self.serverConfigView.serverAddressString = config.serverName;
+    self.serverConfigView.serverPort = config.serverPort;//server config view will use default 22 port
+    self.serverConfigView.accountString = config.account;
+    self.serverConfigView.passwordString = config.password;
+    self.serverConfigView.localPort = config.localPort;//server config view will use default 7070 port
+    self.serverConfigView.remarkString = config.remark;
 }
 
 - (void)serverConfigViewConnectButtonTouched:(SMServerConfigView *)configView
@@ -68,14 +82,16 @@
 
 - (void)serverConfigViewSaveButtonTouched:(SMServerConfigView *)view
 {
+    //Generate a new config
     SMServerConfig *config = [[SMServerConfig alloc] init];
-    //    config.serverName = @"104.128.80.176";
-    config.serverName = @"123123";
-    config.account = @"root";
-    config.password = @"234";
-    config.serverPort = 22;
-    config.localPort = 7070;
+    config.serverName = view.serverAddressString;
+    config.serverPort = view.serverPort;
+    config.account = view.accountString;
+    config.password = view.passwordString;
+    config.localPort = view.localPort;
+    config.remark = view.remarkString;
     
+    //Save to storage
     [[SMServerConfigStorage defaultStorage] addConfig:config];
 }
 
