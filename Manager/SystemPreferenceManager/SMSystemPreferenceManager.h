b77e60d79fe8500ed9ad89a72a2e4373bc6504ae
openthread-mini <openfibers@gmail.com>
2015-06-08 16:07:35 +0800

    added system preference manager

---
+++ Manager/SystemPreferenceManager/SMSystemPreferenceManager.h
@@ -0,0 +1,13 @@
+//
+//  SMSystemPreferenceManager.h
+//  SSHMole
+//
+//  Created by 史江浩 on 6/8/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMSystemPreferenceManager : NSObject
+
+@end
---
+++ Manager/SystemPreferenceManager/SMSystemPreferenceManager.m
@@ -0,0 +1,13 @@
+//
+//  SMSystemPreferenceManager.m
+//  SSHMole
+//
+//  Created by 史江浩 on 6/8/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import "SMSystemPreferenceManager.h"
+
+@implementation SMSystemPreferenceManager
+
+@end
