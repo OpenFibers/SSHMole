763c55edb3035c338d9ecb5551c8c4c29794dccc
OpenFibers <openfibers@gmail.com>
2015-06-22 21:37:43 +0800

    added user settings manager

---
+++ SSHMole/Manager/SMUserProxySettingsManager/SMUserProxySettingsManager.h
@@ -0,0 +1,13 @@
+//
+//  SMUserProxySettingsManager.h
+//  SSHMole
+//
+//  Created by openthread on 6/22/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMUserProxySettingsManager : NSObject
+
+@end
---
+++ SSHMole/Manager/SMUserProxySettingsManager/SMUserProxySettingsManager.m
@@ -0,0 +1,13 @@
+//
+//  SMUserProxySettingsManager.m
+//  SSHMole
+//
+//  Created by openthread on 6/22/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import "SMUserProxySettingsManager.h"
+
+@implementation SMUserProxySettingsManager
+
+@end
