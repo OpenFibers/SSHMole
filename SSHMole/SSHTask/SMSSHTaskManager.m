dca9c79ab93a88513969294b7537f8a12f19d3fa
OpenFibers <openfibers@gmail.com>
2015-06-29 23:48:07 +0800

    SMSSHTaskManager fixed connectingConfig method return unconnected config

--- SSHMole/SSHTask/SMSSHTaskManager.m
+++ SSHMole/SSHTask/SMSSHTaskManager.m
@@ -53,6 +53,9 @@
     _currentTask.shouldLogTaskStdOut = YES;
     _currentTask.callback = ^(SMSSHTaskStatus status, NSError *error) {
         callback(status, error);
+        //Error occurred 和 disconnect 的 callback 没有时序保证
+        //所以在Error occurred 或 disconnect 时不会clear _currentTask
+        //防止后到的状态无法回调
     };
     [_currentTask connect];
 }
@@ -70,7 +73,13 @@
 
 - (SMServerConfig *)connectingConfig
 {
-    return _currentTask.config;
+    if (_currentTask.connected || _currentTask.connectionInProgress)
+    {
+        return _currentTask.config;
+    }
+    
+    //有时_currentTask已断开，但_currentTask不是nil，这时强制返回nil
+    return nil;
 }
 
 @end
