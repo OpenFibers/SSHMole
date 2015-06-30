be48a5f2d07d25a2b14d2fa5a7044aeea62ffdec
OpenFibers <openfibers@gmail.com>
2015-06-30 23:54:28 +0800

    added disconnectForAppTermination

---
+++ SSHMole/ViewControllers/SMStatusBarController/SMStatusBarUserDefaultsManager.h
@@ -0,0 +1,13 @@
+//
+//  SMStatusBarUserDefaultsManager.h
+//  SSHMole
+//
+//  Created by openthread on 6/30/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMStatusBarUserDefaultsManager : NSObject
+
+@end
---
+++ SSHMole/ViewControllers/SMStatusBarController/SMStatusBarUserDefaultsManager.m
@@ -0,0 +1,13 @@
+//
+//  SMStatusBarUserDefaultsManager.m
+//  SSHMole
+//
+//  Created by openthread on 6/30/15.
+//  Copyright (c) 2015 openthread. All rights reserved.
+//
+
+#import "SMStatusBarUserDefaultsManager.h"
+
+@implementation SMStatusBarUserDefaultsManager
+
+@end
