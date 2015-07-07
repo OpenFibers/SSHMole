c20aa541b45245d81efb413bf5c27f9ad85444c5
OpenFibers <openfibers@gmail.com>
2015-07-08 01:25:43 +0800

    SMIPAddressHelper added comments

--- SSHMole/Helpers/SMIPAddressHelper/SMIPAddressHelper.h
+++ SSHMole/Helpers/SMIPAddressHelper/SMIPAddressHelper.h
@@ -14,18 +14,48 @@
 
 #pragma mark IPv4
 
+/**
+ *  获取当前连接的网络中最优先网络的第一个IPv4地址。如果没有网络连接则返回nil。
+ *
+ *  @return 当前连接的网络中最优先网络的第一个IPv4地址。如果没有网络连接则返回nil。
+ */
 + (NSString *)primaryNetworkIPv4AddressFromSystemConfiguration;
 
+/**
+ *  获取当前连接的网络中最优先网络的全部IPv4地址。如果没有网络连接则返回nil。
+ *
+ *  @return 当前连接的网络中最优先网络的全部IPv4地址。如果没有网络连接则返回nil。
+ */
 + (NSArray *)primaryNetworkIPv4AddressArrayFromSystemConfiguration;
 
+/**
+ *  获取当前连接的网络中最优先网络的全部IPv4地址及mask、广播地址。如果没有网络连接则返回nil。
+ *
+ *  @return 当前连接的网络中最优先网络的全部IPv4地址及mask、广播地址。如果没有网络连接则返回nil。
+ */
 + (NSDictionary *)primaryNetworkIPv4AddressInfoFromSystemConfiguration;
 
 #pragma mark IPv6
 
+/**
+ *  获取当前连接的网络中最优先网络的第一个IPv6地址。如果没有网络连接则返回nil。
+ *
+ *  @return 当前连接的网络中最优先网络的第一个IPv6地址。如果没有网络连接则返回nil。
+ */
 + (NSString *)primaryNetworkIPv6AddressFromSystemConfiguration;
 
+/**
+ *  获取当前连接的网络中最优先网络的全部IPv6地址。如果没有网络连接则返回nil。
+ *
+ *  @return 当前连接的网络中最优先网络的全部IPv6地址。如果没有网络连接则返回nil。
+ */
 + (NSArray *)primaryNetworkIPv6AddressArrayFromSystemConfiguration;
 
+/**
+ *  获取当前连接的网络中最优先网络的全部IPv6地址及mask、广播地址。如果没有网络连接则返回nil。
+ *
+ *  @return 当前连接的网络中最优先网络的全部IPv6地址及mask、广播地址。如果没有网络连接则返回nil。
+ */
 + (NSDictionary *)primaryNetworkIPv6AddressInfoFromSystemConfiguration;
 
 #pragma mark - Get IP addresses from NSHost
